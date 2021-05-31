5f60e2820500f88c7be14e1ab1a390fcf69a9158
Thomas Philip <thomasphilipchry@gmail.com>
2021-05-31 13:18:35 +0530

    first setup

---
+++ src/Login.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Login.css'
+
+function Login() {
+    return (
+        <div className="login">
+            
+       
+
+        <h1>I'm the login page</h1>
+
+        
+        
+
+
+        </div>
+    )
+}
+
+export default Login
