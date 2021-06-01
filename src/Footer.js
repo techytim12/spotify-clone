2d6933e22753ad7cce6d07a578d041aca9d3d1d0
Thomas Philip <thomasphilipchry@gmail.com>
2021-06-01 13:54:59 +0530

    Footer done

--- src/Footer.js
+++ src/Footer.js
@@ -1,20 +1,46 @@
 import React from 'react'
 import './Footer.css';
-import PlayCircleOutlineIcon from "@material-ui/icons"
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import { Grid, Slider } from '@material-ui/core';
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
 
 function Footer() {
     return (
         <div className="footer">
             <div class="footer__left">
-                <p>Album and song details</p>
+                <img className="footer__albumLogo" 
+                src="https://a10.gaanacdn.com/images/albums/8/3505508/crop_175x175_1604396676_3505508.jpg" alt="" />
+                <div className="footer__songInfo">
+                    <h4>Wish</h4>
+                    <p>NJ, Mary Ann Alexander</p>
+                </div>
             </div>
 
             <div class="footer__center">
-                <p>Player Controls</p>
+                <ShuffleIcon className="footer__green" />
+                <SkipPreviousIcon className="footer__icon" />
+                <PlayCircleOutlineIcon fontSize="large" className="footer__icon" />
+                <SkipNextIcon className="footer__icon" />
+                <RepeatIcon className="footer__green" />
             </div>
 
             <div class="footer__right">
-                <p>Volume controls</p>
+                <Grid container spacing={2}>
+                    <Grid item>
+                        <PlaylistPlayIcon />
+                    </Grid>
+                    <Grid item>
+                        <VolumeDownIcon />
+                    </Grid>
+                    <Grid item xs>
+                        <Slider />
+                    </Grid>
+                </Grid>
             </div>
             
         </div>
